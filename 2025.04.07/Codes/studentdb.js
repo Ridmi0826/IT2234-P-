416d9b403eeed42ba7b3b99e86d4057d21180e6f
Ridmi Samarasundara <ridmisamarasundara@gmail.com>
2025-04-10 18:03:12 +0530

    Add files via upload

---
+++ 2025.04.07/Codes/studentdb.js
@@ -0,0 +1,11 @@
+let students = [
+    {regno:'2021ICT110',name:'Lucifer',age:22,gender:'male',course:'IT'},
+    {regno:'2021BIO111',name:'Rose',age:21,gender:'female',course:'BIO'},
+    {regno:'2021ICT112',name:'Ram',age:23,gender:'male',course:'IT'},
+    {regno:'2021AMC113',name:'Fiona',age:20,gender:'female',course:'Maths'},
+    {regno:'2021ICT113',name:'Kenny',age:22,gender:'female',course:'IT'},
+    {regno:'2021BIO114',name:'Dinu',age:23,gender:'male',course:'BIO'}
+
+];
+
+module.exports=students;
